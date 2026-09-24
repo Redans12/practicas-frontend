4e7724f2b8cea0efd0af6fd031c8dcef0d1a86d0
Andrés Ibarrola <andres.daniel.or@gmail.com>
2026-09-24 01:19:56 -0600

    feat(js): add variable naming and types exercise

---
+++ js/ejercicio-1/ejercicio-1.js
@@ -0,0 +1,7 @@
+const userName = "Pedro";
+const UserName = "Pedro";
+const user_name = "Pedro";
+// kebab-case no aplica a variables JS, solo a strings/clases CSS: "user-name"
+
+let contador = 0;
+const PI = 3.1416;
